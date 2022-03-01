720b82bd088bb3eef62eb52d0322bacd02e4ef63
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-03-01 20:27:48 +0600

    HomeWork2 begins, HomeWork1 task9 fixed

--- HomeWorkSeminars/Seminar1/Sem1Task9/Program.cs
+++ HomeWorkSeminars/Seminar1/Sem1Task9/Program.cs
@@ -7,19 +7,11 @@
 
         if (number < 100) Console.WriteLine("The number given has no third digit"); // checking
         else {
-            int length = 0;
-            int tempNum = number;
-
-            while (tempNum > 0) // calculates the number's length
+            
+            while (number >= 1000) // calculates the number's length
             {
-                tempNum /= 10;
-                length++;
-            }
-
-            for (int i = 0; i < length - 3; i++) // finds the third digit
-            {
-                number /= 10;
-            }
+                number /= 10;              
+            }         
 
             Console.WriteLine("The third digit of the number given is: " + number % 10); 
         }
---
+++ HomeWorkSeminars/Seminar2/Sem2Hometask1/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Sem2HomeTask1 { // Показать числа от -N до N
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int i = -number;
+
+        while (i <= number) Console.Write(i++ + " ");
+    }
+
+}
