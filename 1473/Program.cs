ef1ca1873db481bfb41f5a513f12539329c705d0
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-03-03 17:59:51 +0600

    cherry_pick_up_1463 task from leetcode.com has been solved and added to the project

--- 1473/Program.cs
+++ 1473/Program.cs
@@ -1,4 +1,4 @@
-﻿public class PaintHouseIII1473
+﻿public class PaintHouseIII1473 // accepted (speed: 112ms, ultra-fast, beats 97,7% of c# submissions)
 {
     public static int[,,] memoTable;
     public static int MaxValue = 10000001;
@@ -9,38 +9,42 @@
         int target = 3;
         int m = 4, n = 3;
 
-        Console.WriteLine(minCost(houses, cost, m, n, target));
+        Console.WriteLine(minCost(houses, cost, m, n, target)); // method call
     }
 
     public static int minCost(int[] houses, int[,] cost, int m, int n, int target)
     {
-        memoTable = new int[m, n + 1, target + 1];
+        memoTable = new int[m, n + 1, target + 1]; // initialization of dp memoTable
 
-        int result = MaxValue;     
+        int result = MaxValue; // a convenient value< it's greater than potentially possible maxCost and less than int.MaxValue so than its value cannot exceed the int limit     
         
-        result = Math.Min(result, recursiveSeeker(m - 1, 0, target, houses, cost, m, n));
+        result = Math.Min(result, recursiveSeeker(m - 1, 0, target, houses, cost, m, n)); // initial rec call
         
-        return result == MaxValue ? -1 : result;
+        return result == MaxValue ? -1 : result; // return value, if task has no solution we return -1
     }
 
-    public static int recursiveSeeker(int i, int j, int k, int[] houses, int[,] cost, int m, int n) 
-    {
-        if (i == 0) Console.WriteLine("i = 0 " + " k = " + k);
-        if (i == -1 || k < 0) return k == 0 ? 0 : MaxValue;
+    // rec (i, j, k) signifies: minCost of painting i adjacent houses (beginning from index 0 in houses array) where the rightmost house painted in colour j 
+    // in order to achieve exactly k neighbourhoods 
+    public static int recursiveSeeker(int i, int j, int k, int[] houses, int[,] cost, int m, int n) // recursive dynamic programming
+    {        
+        if (i == -1 || k < 0) return k == 0 ? 0 : MaxValue; // base cases: 1. if we at i == -1 then k must be equal to 0,
+                                                            // if i != 0 and k < 0 then this is the wrong branch with no solution
         
-
-        if (memoTable[i, j, k] != 0) return memoTable[i, j, k];
+        if (memoTable[i, j, k] != 0) return memoTable[i, j, k]; // if we has already calculated the minCost at this step then we just use it
 
         int result = MaxValue;
 
-        if (houses[i] != 0) return recursiveSeeker(i - 1, houses[i], k - (j == houses[i] ? 0 : 1), houses, cost, m, n);
-        for (int colour = 1; colour < n + 1; colour++)
+        if (houses[i] != 0) return recursiveSeeker(i - 1, houses[i], k - (j == houses[i] ? 0 : 1), houses, cost, m, n); // if the house has been already painted then
+                                                                                                                        // we cant pain it again and pay no cost
+        for (int colour = 1; colour < n + 1; colour++) // cycling over all colours given
         {
-            int nextStepCost = recursiveSeeker(i - 1, colour, k - (j == colour ? 0 : 1), houses, cost, m, n);            
+            int nextStepCost = recursiveSeeker(i - 1, colour, k - (j == colour ? 0 : 1), houses, cost, m, n); // next step of recursion (i -> i - 1), if these adjacent houses (i and i - 1)
+                                                                                                              // painted in the same colour then when we proceeding
+                                                                                                              // to the next rec step we do not decrease k (target) value,
+                                                                                                              // otherwise, if the colours differ -> we decrease k by one: k -> k-1
             result = Math.Min(result, cost[i, colour - 1] + nextStepCost);
         }   
 
-        return memoTable[i, j, k] = result;
+        return memoTable[i, j, k] = result; // return calculated memo value
     }
-
 }
---
+++ Cherry_Pickup_II_1463/Program.cs
@@ -0,0 +1,59 @@
+﻿public class CherryPickupII1463 // accepted (speed: 108ms, fast, beats 86,11% of C# submissions)
+{
+
+    public static int[,,] memoTable; // memoTable for dynamic programming
+
+    static void Main(string[] args)
+    {
+        int[,] grid = new int[,] { { 1, 0, 0, 0, 0, 0, 1 }, { 2, 0, 0, 0, 0, 3, 0 }, { 2, 0, 9, 0, 0, 0, 0 }, {0, 3, 0, 5, 4, 0, 0 }, { 1, 0, 2, 3, 0, 0, 6 } };
+
+        Console.WriteLine(CherryPickup(grid)); // base method call     
+    }
+
+    public static int CherryPickup(int[,] grid) // base method
+    {
+        memoTable = new int [grid.GetLength(0), grid.GetLength(1), grid.GetLength(1)]; // initialization of memoTable
+
+        // Console.WriteLine("columns: " + grid.GetLength(0) + " rows: " + grid.GetLength(1)); // checking
+
+        for (int i = 0; i < grid.GetLength(0); i++) 
+            for (int j = 0; j < grid.GetLength(1); j++) 
+                for (int k = 0; k < grid.GetLength(1); k++) 
+                    memoTable[i, j, k] = -1;     
+
+        return recursiveSeeker(0, 0, grid.GetLength(1) - 1, grid); // first recursive call
+    }
+
+    public static int recursiveSeeker(int i, int j, int k, int[,] grid) // recursive dynamic programming nethod
+    {
+        if (j < 0 || j >= grid.GetLength(1) || k < 0 || k >= grid.GetLength(1)) return 0; // if we are out of range
+
+        if (memoTable[i, j, k] != -1) return memoTable[i, j, k]; // if current step has already been calculated then
+                                                                 // we just use its cashed in the memoTable value
+
+        int result = 0; // sum of next step max cherries number and max cherries picked up at the current step
+
+        result += grid[i, j]; // we always add a first robot's number of picked up cherries
+
+        if (j != k) result += grid[i, k]; // if robots do not stay in the same cell we add two heap of cherries to the result
+
+        if (i != grid.GetLength(0) - 1) // when we are not out of range in terms of columns
+        {
+            int maxNextStepCherriesNumber = 0; // max cherries number on the next step
+
+            for (int newJ = j - 1; newJ <= j + 1; newJ++) // cycling over all possible next-step cells (1st robot)
+            {
+                for (int newK = k - 1; newK <= k + 1; newK++) // cycling over all possible next-step cells (2nd robot)
+                {
+                    maxNextStepCherriesNumber = Math.Max(maxNextStepCherriesNumber, recursiveSeeker(i + 1, newJ, newK, grid)); // searching for the max
+                                                                                                                               // next step cherries number
+                }
+            }
+
+            result += maxNextStepCherriesNumber; // getting the result
+        }
+
+        return memoTable[i, j, k] = result; // return memoTable value
+    }
+
+}
