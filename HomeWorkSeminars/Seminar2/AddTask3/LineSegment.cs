1325e41e56793fcdf0cba54c3daf57a66aa9968e
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-03-04 22:17:29 +0600

    minor changes in AddTask3 Sem3

--- HomeWorkSeminars/Seminar2/AddTask3/LineSegment.cs
+++ HomeWorkSeminars/Seminar2/AddTask3/LineSegment.cs
@@ -13,29 +13,32 @@ internal class LineSegment
     { 
     }
 
-    public LineSegment(KeyValuePair<double, double> startPoint, KeyValuePair<double, double> endPoint) 
+    public LineSegment(KeyValuePair<double, double> startPoint, KeyValuePair<double, double> endPoint) // constructor of LineSegments by two points
     {
-        if (startPoint.Key == endPoint.Key && startPoint.Value == endPoint.Value) {
+        if (startPoint.Key == endPoint.Key && startPoint.Value == endPoint.Value) { // the same points throw an ArgumentException
             throw new ArgumentException("The coordinates of points cannot match ");
         }
 
         this.startPoint = startPoint;
         this.endPoint = endPoint;
 
-        Console.WriteLine("A line segment with StartPoint: (" + startPoint.Key + "," + startPoint.Value +
+        Console.WriteLine("A line segment with StartPoint: (" + startPoint.Key + "," + startPoint.Value + // printing info about creation
             ") and EndPoint: (" + endPoint.Key + "," + endPoint.Value + ") has been created");
     }
 
-    public bool ContainsPoint(KeyValuePair<double, double> point) => new StraightLine(this, false).ContainsPoint(point) && 
+    public bool ContainsPoint(KeyValuePair<double, double> point) => new StraightLine(this, false).ContainsPoint(point) && // checks if the line segment contains
+                                                                                                                           // the point given
         Math.Min(startPoint.Key, endPoint.Key) <= point.Key && point.Key <= Math.Max(startPoint.Key, endPoint.Key);
-    public static bool ContainsPointOfIntersection(LineSegment segment1, LineSegment segment2) 
+    public static bool ContainsPointOfIntersection(LineSegment segment1, LineSegment segment2) // checks if the both line segments contain the point of
+                                                                                               // intersection of two straight lines created by these segments
     {
-        StraightLine ABline = new StraightLine(segment1, true);
+        StraightLine ABline = new StraightLine(segment1, true); // creation of a straight line that corresponds to a line segment given
         StraightLine CDline = new StraightLine(segment2, true);
 
-        if (StraightLine.IsParallel(ABline, CDline))
+        if (StraightLine.IsParallel(ABline, CDline)) // if the lines are parallel -> there is no point of intersection of them
         {
-            Console.WriteLine("As the line AB is Parallel to the line CD -> the point of intersection does not exist in the context of the task given");
+            Console.WriteLine("As the line AB is Parallel to the line CD or AB equals CD -> " +
+                "the point of intersection does not exist in the context of the task given");
             return false; 
         }
       
@@ -44,8 +47,10 @@ internal class LineSegment
         Console.WriteLine("Point of intersection coordinates: (" +
             pointOfIntersection.Key + "," + pointOfIntersection.Value + ")");
 
-        bool flag = segment1.ContainsPoint(pointOfIntersection) && segment2.ContainsPoint(pointOfIntersection);
-
+        bool flag = segment1.ContainsPoint(pointOfIntersection) && segment2.ContainsPoint(pointOfIntersection); // if both line segments contain
+                                                                                                                // the point of intersection (that exists) then ->
+                                                                                                                // we can define the number of coordinate quarter
+                                                                                                                // it's located in
         if (flag)
         {
             AddTask3.Quarter(pointOfIntersection);
--- HomeWorkSeminars/Seminar2/AddTask3/StraightLine.cs
+++ HomeWorkSeminars/Seminar2/AddTask3/StraightLine.cs
@@ -6,47 +6,46 @@ using System.Threading.Tasks;
 
 internal class StraightLine
 {
-    public double xCoefficient;
-    public double yCoefficient;
-    public double shift;
+    public double xCoefficient; // >A<x + Bx = k
+    public double yCoefficient; // Ax + >B<y = k
+    public double shift;        // Ax + By = >k<
     public StraightLine()
     {
     }
-    public StraightLine(LineSegment segment, bool isPrint)
+    public StraightLine(LineSegment segment, bool isPrint) // constructor with boolean flag of printing
     {
         if (segment.startPoint.Key == segment.endPoint.Key) // straight line that is parallel to Ordinate axis
         {
             xCoefficient = 1;
             yCoefficient = 0;
             shift = segment.startPoint.Key;
-            return;
-        }
 
-        if (segment.startPoint.Value == segment.endPoint.Value) // straight line that is parallel to Abscissa axis
+        } else if (segment.startPoint.Value == segment.endPoint.Value) // straight line that is parallel to Abscissa axis
         {
             xCoefficient = 0;
             yCoefficient = 1;
-            shift = segment.startPoint.Value;
-            return;
-        }
-
-        this.yCoefficient = 1;
-        this.xCoefficient = - (segment.startPoint.Value - segment.endPoint.Value) / (segment.startPoint.Key - segment.endPoint.Key);
-        this.shift = (segment.startPoint.Value * segment.endPoint.Key - segment.endPoint.Value * segment.startPoint.Key) /
-            (segment.endPoint.Key - segment.startPoint.Key);
-
-        if (xCoefficient < 0 && yCoefficient < 0 && shift < 0) 
+            shift = segment.startPoint.Value;           
+        } else
         {
-            Console.WriteLine("lala");
-            xCoefficient = -xCoefficient;
-            yCoefficient = -yCoefficient;
-            shift = -shift;
+            this.yCoefficient = 1;
+            this.xCoefficient = -(segment.startPoint.Value - segment.endPoint.Value) / (segment.startPoint.Key - segment.endPoint.Key);
+            this.shift = (segment.startPoint.Value * segment.endPoint.Key - segment.endPoint.Value * segment.startPoint.Key) /
+                (segment.endPoint.Key - segment.startPoint.Key);
+
+            if (xCoefficient < 0 && yCoefficient < 0 && shift < 0) // for convenient math and print
+            {
+                Console.WriteLine("lala");
+                xCoefficient = -xCoefficient;
+                yCoefficient = -yCoefficient;
+                shift = -shift;
+            }
         }
 
-        if (isPrint) Console.WriteLine("Straight line with the equation: " + this.xCoefficient + "*x " + (this.yCoefficient > 0 ? "+ " : "- ") + Math.Abs(this.yCoefficient) +
-            "*y = " + this.shift + " has been created");
+        if (isPrint) Console.WriteLine("Straight line with the equation: " + this.xCoefficient + "*x " + (this.yCoefficient >= 0 ? "+ " : "- ") + Math.Abs(this.yCoefficient) +
+            "*y = " + this.shift + " has been created"); // printing
     }
-    public static KeyValuePair<double, double> PointOfIntersection(StraightLine line1, StraightLine line2)
+    public static KeyValuePair<double, double> PointOfIntersection(StraightLine line1, StraightLine line2) // searching for the point of intersection
+                                                                                                           // line1 and line2
     {
         if (IsParallel(line1, line2))
         {
@@ -60,21 +59,21 @@ internal class StraightLine
                                                                                                          // straight lines are not parallel
             (line2.yCoefficient * line1.xCoefficient - line1.yCoefficient * line2.xCoefficient);
 
-
-
         pointOfIntercection = new KeyValuePair<double, double>(xOfIntercection, line1.GetY(xOfIntercection));
 
         return pointOfIntercection;
     }
 
-    public double GetY(double x) {
+    public double GetY(double x) { // tries to find Y value
         if (yCoefficient != 0) return (shift - xCoefficient * x) / yCoefficient;
         else 
         {
             throw new ArgumentException("yCoefficient is equal to zero!!!");
         }
     }
-    public bool ContainsPoint(KeyValuePair<double, double> point) => xCoefficient * point.Key + yCoefficient * point.Value == shift;
-    public static bool IsParallel(StraightLine line1, StraightLine line2) => line1.xCoefficient * line2.yCoefficient == line1.yCoefficient * line2.xCoefficient;   
+    public bool ContainsPoint(KeyValuePair<double, double> point) => xCoefficient * point.Key + yCoefficient * point.Value == shift; // checks if the straight line contains
+                                                                                                                                     // the point given
+    public static bool IsParallel(StraightLine line1, StraightLine line2) => line1.xCoefficient * line2.yCoefficient == // checks if line1 || line2
+        line1.yCoefficient * line2.xCoefficient;   
 }
 
