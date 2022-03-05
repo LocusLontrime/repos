9e4078180ee8254b4680f6eef66b382fa37f2424
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2022-03-05 11:18:38 +0600

    some minor changes

--- HomeWorkSeminars/Seminar3/Sem3HomeTask3/Program.cs
+++ HomeWorkSeminars/Seminar3/Sem3HomeTask3/Program.cs
@@ -1,4 +1,4 @@
-﻿public class Sem3HomeTask3 
+﻿public class Sem3HomeTask3 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
 {
 
     static void Main(string[] args)
@@ -7,7 +7,6 @@
         Console.WriteLine("Enter a number: ");
         int number = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= number; i++) Console.Write(i * i * i + " ");
-  
+        for (int i = 1; i <= number; i++) Console.Write(i * i * i + " "); 
     }
 }
